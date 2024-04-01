0a958257b118b3d1273ab17a7c260ae112a921f3
SirIyanu <105286160+SirIyanu@users.noreply.github.com>
2024-04-01 08:41:18 +0100

    dashboard layout

--- src/app/pages/Dashboard/index.jsx
+++ src/app/pages/Dashboard/index.jsx
@@ -1,3 +1,12 @@
+import style from "./style.module.css";
 export const DashboardPage = () => {
-  return <>dashboard</>;
+  return (
+    <section className={style.container}>
+      <section className={style["content-wrapper"]} role="content">
+        <aside>aside</aside>
+        <main>main</main>
+      </section>
+      <footer className={style.footer}>footer</footer>
+    </section>
+  );
 };
