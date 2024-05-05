d2ddbd74602c48ca28119240809b12100433e0c0
Olawoye Iyanuluwa <iyolawoye@gmail.com>
2024-05-05 22:46:59 +0100

    added new song route

--- src/app/Service/songs.jsx
+++ src/app/Service/songs.jsx
@@ -2,12 +2,12 @@ import { faker } from "@faker-js/faker";
 import { routes } from "../route";
 
 export const mostListenedToApi = async () => {
-  const resp = await fetch("http://localhost:3001/songs");
+  const resp = await fetch("https://spotify-api-snowy-two.vercel.app/songs");
   const data = (await resp.json()) ?? [];
   return data.splice(0, 8);
 };
 
 export const episodesApi = async () => {
-  const resp = await fetch("http://localhost:3001/songs");
+  const resp = await fetch("https://spotify-api-snowy-two.vercel.app/songs");
   return resp.json();
 };
